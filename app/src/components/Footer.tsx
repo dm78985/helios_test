4c9424905b5f3b865699254c23b401ea1ad212b2
Daniel millar <danielmillar@Daniels-MacBook-Pro.local>
2024-10-12 10:32:31 +0100

    chore: add disclaimer text

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,5 +1,13 @@
 import "@/styles/footer.css"
 
 export const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <footer>
+      <div>
+        This has no affiliation with the brands included. This is purely for
+        practise.
+      </div>
+      <div>Navigation</div>
+    </footer>
+  )
 }
